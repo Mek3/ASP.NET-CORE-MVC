bdc4d2a4a5ec95a29c72eb20ab8a01e3592d22dd
Mohammed <mek3>
2023-03-08 20:37:18 +0100

    ADD: proyecto 3: Entity Framework Core y Seguridad

---
+++ TareasMVC/Migrations/20230308180841_AdminRol.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TareasMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminRol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"IF NOT EXISTS (SELECT Id FROM AspNetRoles WHERE Id ='ca4411b8-8747-4ab6-bf85-e904e74a82aa')
+                                    BEGIN
+	                                    INSERT AspNetRoles (Id, [Name], [NormalizedName]) 
+	                                    VALUES ('ca4411b8-8747-4ab6-bf85-e904e74a82aa', 'admin', 'ADMIN')
+                                    END");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE AspNetRoles WHERE Id ='ca4411b8-8747-4ab6-bf85-e904e74a82aa'");
+        }
+    }
+}
